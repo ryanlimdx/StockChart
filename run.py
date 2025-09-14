a80af935e36e9647526c22f317634bd48aaddb2d
ryan <ryanlimdx@gmail.com>
2025-09-14 15:26:22 +0800

    Feat: Allow processing other tickers from command line

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
+import argparse
 from stockcharts.app import StockChartApp
 
 if __name__ == '__main__':
-    app = StockChartApp()
-    app.run(debug=True)
+    parser = argparse.ArgumentParser(description="Run the StockChart Dash application.")
+    parser.add_argument(
+        '--ticker',
+        type=str,
+        default='NVDA',
+        help='The stock ticker symbol to display (e.g., AAPL, GOOG). Defaults to NVDA.'
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        default=False,
+        help='Enable debug mode. Defaults to True.'
+    )
+    args = parser.parse_args()
+
+    app = StockChartApp(ticker=args.ticker)
+    app.run(debug=args.debug)
